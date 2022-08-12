00570a558159b423e2b52a55e8e46f5804a425cd
harsh <harshmangal21082002@gmail.com>
2022-08-13 01:32:15 +0530

    first commit

--- Responsive Portfolio Website/script.js
+++ Responsive Portfolio Website/script.js
@@ -11,12 +11,7 @@ $(document).ready(function(){
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-
+   
     
     // toggle menu/navbar script
     
